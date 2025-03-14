30593565ae3f486ae44b08f9699a72f0e31085b9
ITSolution1890 <itsolutionwebdev@gmail.com>
2025-03-14 22:11:43 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+# Python program to take input from user
+
+# Taking input from user
+a = int(input())
+
+# Taking input from user
+b = int(input())
+
+# Multiplying and storing result
+c = a * b
+
+# Printing the result
+print(c)
